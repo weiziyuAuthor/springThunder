1f1236fa7bc881427cf16156770440b6f2517870
ziyu.wei <ziyu.wei@tendcloud.com>
2016-11-28 19:37:28 +0800

    双向检查的单例模式

---
+++ springThunder/src/main/java/com/yu/jvm/thread/Singleton.java
@@ -0,0 +1,22 @@
+package com.yu.jvm.thread;
+
+public class Singleton {
+
+  private volatile static Singleton instance;
+
+  public static Singleton getInstance() {
+    if (instance == null) {
+      synchronized (Singleton.class) {
+        if (instance == null) {
+          instance = new Singleton();
+        }
+      }
+    }
+    return instance;
+  }
+
+
+  public static void main(String[] args) {
+    Singleton.getInstance();
+  }
+}
