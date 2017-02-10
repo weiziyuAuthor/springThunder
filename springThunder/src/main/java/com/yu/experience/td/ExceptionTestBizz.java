2ecec44a189ef1953fbff5231df5ae83a13c66a4
ziyu.wei <ziyu.wei@tendcloud.com>
2017-02-10 14:25:49 +0800

    建议内部throw,不打印日志，防止日志泛滥
    给出导入工程的步骤

--- springThunder/src/main/java/com/yu/experience/td/ExceptionTestBizz.java
+++ springThunder/src/main/java/com/yu/experience/td/ExceptionTestBizz.java
@@ -1,12 +1,22 @@
 package com.yu.experience.td;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ExceptionTestBizz {
 
-  public static void test() throws ExceptionTest {
-    try {
-      int i = 1 / 0;
-    } catch (Exception e) {
-      throw new ExceptionTest("error occurs", e);
-    }
-  }
+	private static Logger logger = LoggerFactory
+			.getLogger(ExceptionTestBizz.class);
+
+	public static void test() throws ExceptionTest {
+		try {
+			String string = "";
+			int i = 1 / 0;
+		} catch (Exception e) {
+//			logger.error("ExceptionTestBizz error occurs", e);
+//			throw e;
+
+			 throw new ExceptionTest("inner find error", e);
+		}
+	}
 }
--- springThunder/src/main/java/com/yu/experience/td/ExceptionTestMain.java
+++ springThunder/src/main/java/com/yu/experience/td/ExceptionTestMain.java
@@ -1,12 +1,29 @@
 package com.yu.experience.td;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author weizy
+ *
+ * 2017年2月10日    下午2:24:43
+ * 
+ * 建议内部throw,不打印日志，防止日志泛滥
+ */
 public class ExceptionTestMain {
 
+	private static Logger logger = LoggerFactory
+			.getLogger(ExceptionTestMain.class);
+	
   public static void main(String[] args) {
     try {
       ExceptionTestBizz.test();
     } catch (ExceptionTest e) {
-      e.printStackTrace();
+//      e.printStackTrace();
+    	System.out.println("------");
+    	logger.error("caller find error", e);
+    	
     }
   }
 
